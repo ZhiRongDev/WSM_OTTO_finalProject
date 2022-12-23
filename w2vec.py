ac81bbe71a79b0cc4b993e9c800eeb768843e70a
zhi-rong <jordan990301@gmail.com>
2022-12-23 22:39:59 +0800

    try Fast Similarity Queries with Annoy and Word2Vec in official way

--- w2vec.py
+++ w2vec.py
@@ -5,28 +5,19 @@ from gensim.models import Word2Vec
 train = pl.read_parquet('./data/test/train.parquet')
 test = pl.read_parquet('./data/test/test.parquet')
 
-
 sentences_df = pl.concat([train, test]).groupby('session').agg(
     pl.col('aid').alias('sentence')
 )
 
 sentences = sentences_df['sentence'].to_list()
 
-w2vec = Word2Vec(sentences=sentences, vector_size=32, min_count=1, workers=4)
+w2vec = Word2Vec(sentences=sentences, vector_size=100, min_count=1, workers=4)
 
 ########
 from gensim.similarities.annoy import AnnoyIndexer
 
 # 100 trees are being used in this example
-annoy_index = AnnoyIndexer(w2vec, 10)
-# Derive the vector for the word "science" in our model
-vector = w2vec.wv[0]
-# The instance of AnnoyIndexer we just created is passed
-approximate_neighbors = w2vec.wv.most_similar([vector], topn=11, indexer=annoy_index)
-# Neatly print the approximate_neighbors and their corresponding cosine similarity values
-print("Approximate Neighbors")
-for neighbor in approximate_neighbors:
-    print(neighbor)
+annoy_index = AnnoyIndexer(w2vec, 100)
 
 ########
 
@@ -61,7 +52,26 @@ for AIDs, types in zip(test_session_AIDs, test_session_types):
         # let's grab the most recent aid
         most_recent_aid = AIDs[0]
         
+        # print(most_recent_aid)
+    
         # and look for some neighbors!
-        nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
-                        
+        # nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
+        vector = w2vec.wv[most_recent_aid]
+        nns = [i for i, j in w2vec.wv.most_similar([vector], topn=21, indexer=annoy_index)]
+        nns.pop(0)
+
         labels.append((AIDs+nns)[:20])
+
+labels_as_strings = [' '.join([str(l) for l in lls]) for lls in labels]
+
+predictions = pd.DataFrame(data={'session_type': test_session_AIDs.index, 'labels': labels_as_strings})
+
+prediction_dfs = []
+
+for st in session_types:
+    modified_predictions = predictions.copy()
+    modified_predictions.session_type = modified_predictions.session_type.astype('str') + f'_{st}'
+    prediction_dfs.append(modified_predictions)
+
+submission = pd.concat(prediction_dfs).reset_index(drop=True)
+submission.to_csv('submission.csv', index=False)
