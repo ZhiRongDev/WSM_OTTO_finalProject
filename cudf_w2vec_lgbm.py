7c689546119df9cb9140039961ed84e18754281a
zhi-rong <jordan990301@gmail.com>
2022-12-30 23:34:57 +0800

    cudf + w2vec + lgbm fix pipline

--- cudf_w2vec_lgbm.py
+++ cudf_w2vec_lgbm.py
@@ -1,16 +1,18 @@
 import time
-
 import pandas as pd
+import polars as pl
+import numpy as np
+from collections import defaultdict
 import cudf
 cudf.set_option("default_integer_bitwidth", 32)
 cudf.set_option("default_float_bitwidth", 32)
 
-import polars as pl
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
 
-# train = pl.read_parquet('./data/test/train.parquet')
-train = pl.read_parquet('./data/local_validation/test.parquet')
+# train = pl.read_parquet('./data/local_validation/test.parquet')
+train = pl.read_parquet('./data/test/train.parquet')
+test = pl.read_parquet('./data/test/test.parquet')
 
 train = train.with_columns([
     pl.col('session').cast(pl.datatypes.Int32),
@@ -19,7 +21,15 @@ train = train.with_columns([
     pl.col('ts').cast(pl.datatypes.Int64)
 ])
 
-sentences_df = pl.concat([train]).groupby('session').agg(
+test = test.with_columns([
+    pl.col('session').cast(pl.datatypes.Int32),
+    pl.col('type').cast(pl.datatypes.UInt8),
+    pl.col('aid').cast(pl.datatypes.Int32),
+    pl.col('ts').cast(pl.datatypes.Int64)
+])
+
+##### for Word2Vec pretrain
+sentences_df = pl.concat([train, test]).groupby('session').agg(
     pl.col('aid').alias('sentence')
 )
 
@@ -43,104 +53,78 @@ for aid, idx in aid2idx.items():
     index.add_item(idx, w2vec.wv.vectors[idx])
     
 index.build(10)
+#####
 
-import pandas as pd
-import numpy as np
+###
+train_labels = pl.read_parquet('./data/local_validation/test_labels.parquet')
 
-from collections import defaultdict
+def word2vec_candidate(df): 
+    global index
+    session_types = ['clicks', 'carts', 'orders']
+    df_session_AIDs = df.to_pandas().reset_index(drop=True).groupby('session')['aid'].apply(list)
+    df_session_types = df.to_pandas().reset_index(drop=True).groupby('session')['type'].apply(list)
+    
+    candidates = cudf.DataFrame({"session": [], "aid":[]})
 
-session_types = ['clicks', 'carts', 'orders']
-train_session_AIDs = train.to_pandas().reset_index(drop=True).groupby('session')['aid'].apply(list)
-train_session_types = train.to_pandas().reset_index(drop=True).groupby('session')['type'].apply(list)
+    print("candidate calc start")
+    start = time.time()
 
-# labels = []
-# candidates = pl.DataFrame({
-#     "session": [],
-#     "aid": []
-# })
-# candidates = candidates.with_columns([
-#     pl.col('session').cast(pl.datatypes.Int32),
-#     pl.col('aid').cast(pl.datatypes.Int32)
-# ])
+    type_weight_multipliers = {0: 1, 1: 6, 2: 3}
+    for idx, (AIDs, types) in enumerate(zip(df_session_AIDs, df_session_types)):
+        session_num = df_session_AIDs.index[idx]    
+        # print(session_num)
+        if len(AIDs) >= 20:
+            # if we have enough aids (over equals 20) we don't need to look for candidates! we just use the old logic
+            weights=np.logspace(0.1,1,len(AIDs),base=2, endpoint=True)-1
+            aids_temp=defaultdict(lambda: 0)
+            for aid,w,t in zip(AIDs,weights,types): 
+                aids_temp[aid]+= w * type_weight_multipliers[t]
+                
+            sorted_aids=[k for k, v in sorted(aids_temp.items(), key=lambda item: -item[1])]
 
-candidates = cudf.DataFrame({"session": [], "aid":[]})
+            if len(sorted_aids) < 20:
+                AIDs = list(dict.fromkeys(AIDs[::-1]))
+                # let's grab the most recent aid
+                most_recent_aid = AIDs[0]
+                # and look for some neighbors!
+                nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
+                sorted_aids = (sorted_aids + nns)[:20]
 
-print("candidate calc start")
-start = time.time()
+            session_arr = np.full((20), session_num)        
 
-type_weight_multipliers = {0: 1, 1: 6, 2: 3}
-for idx, (AIDs, types) in enumerate(zip(train_session_AIDs, train_session_types)):
-    session_num = train_session_AIDs.index[idx]    
-
-    # print(session_num)
-
-    if len(AIDs) >= 20:
-
-        # if we have enough aids (over equals 20) we don't need to look for candidates! we just use the old logic
-        weights=np.logspace(0.1,1,len(AIDs),base=2, endpoint=True)-1
-        aids_temp=defaultdict(lambda: 0)
-        for aid,w,t in zip(AIDs,weights,types): 
-            aids_temp[aid]+= w * type_weight_multipliers[t]
-            
-        sorted_aids=[k for k, v in sorted(aids_temp.items(), key=lambda item: -item[1])]
-
-        if len(sorted_aids) < 20:
+            session_df = cudf.DataFrame({
+                "session": session_arr,
+                "aid": sorted_aids[:20] 
+            })
+            candidates = cudf.concat([candidates, session_df])
+        else:
+            # here we don't have 20 aids to output -- we will use word2vec embeddings to generate candidates!
             AIDs = list(dict.fromkeys(AIDs[::-1]))
             # let's grab the most recent aid
             most_recent_aid = AIDs[0]
             # and look for some neighbors!
             nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
-            sorted_aids = (sorted_aids + nns)[:20]
+            session_arr = np.full((20), session_num)
+            session_df = cudf.DataFrame({
+                "session": session_arr,
+                "aid": (AIDs+nns)[:20] 
+            })
+            
+            candidates = cudf.concat([candidates, session_df])
 
-        session_arr = np.full((20), session_num)        
+    print("candidate calc end")
+    end = time.time()
+    print(f'執行時間: {end - start} 秒\n')
 
-        df = cudf.DataFrame({
-            "session": session_arr,
-            "aid": sorted_aids[:20] 
-        })
-        candidates = cudf.concat([candidates, df])
-
-
-    else:
-        # here we don't have 20 aids to output -- we will use word2vec embeddings to generate candidates!
-        AIDs = list(dict.fromkeys(AIDs[::-1]))
-        # let's grab the most recent aid
-        most_recent_aid = AIDs[0]
-        # and look for some neighbors!
-        nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
-        
-        session_arr = np.full((20), session_num)
-        
-        df = cudf.DataFrame({
-            "session": session_arr,
-            "aid": (AIDs+nns)[:20] 
-        })
-        
-        candidates = cudf.concat([candidates, df])
-
-print("candidate calc end")
-end = time.time()
-print(f'執行時間: {end - start} 秒\n')
-
-
-candidates = candidates.to_pandas()
-candidates = pl.DataFrame(candidates)
-
-# print(f'{candidates=}')
-candidates = candidates.with_column(pl.col('aid').cumcount().over('session').alias('word2vec_rank') + 1)
-
-candidates = candidates.with_columns([
-    pl.col('session').cast(pl.datatypes.Int32),
-])
-
-train = train.join(candidates, on=['session', 'aid'], how='outer').sort("session")
-
-train_labels = pl.read_parquet('./data/local_validation/test_labels.parquet')
-
-print(f'{train=}')
-print(f'{train_labels=}')
-
-breakpoint()
+    candidates = candidates.to_pandas()
+    candidates = pl.DataFrame(candidates)
+    candidates = candidates.with_column(pl.col('aid').cumcount().over('session').alias('word2vec_rank') + 1)
+    candidates = candidates.with_columns([
+        pl.col('session').cast(pl.datatypes.Int32),
+    ])
+     
+    df = df.join(candidates, on=['session', 'aid'], how='outer').sort("session")
+    return df
 
 def add_action_num_reverse_chrono(df):
     return df.select([
@@ -168,9 +152,11 @@ def apply(df, pipeline):
         df = f(df)
     return df
 
-pipeline = [add_action_num_reverse_chrono, add_session_length, add_log_recency_score, add_type_weighted_log_recency_score]
+pipeline = [add_action_num_reverse_chrono, add_session_length, add_log_recency_score, add_type_weighted_log_recency_score, word2vec_candidate]
 
 train = apply(train, pipeline)
+
+# print(train.columns)
 
 type2id = {"clicks": 0, "carts": 1, "orders": 2}
 
@@ -206,7 +192,7 @@ ranker = LGBMRanker(
     importance_type='gain',
 )
 
-feature_cols = ['aid', 'type', 'action_num_reverse_chrono', 'session_length', 'log_recency_score', 'type_weighted_log_recency_score']
+feature_cols = ['aid', 'type', 'action_num_reverse_chrono', 'session_length', 'log_recency_score', 'type_weighted_log_recency_score', 'word2vec_rank']
 target = 'gt'
 
 ranker = ranker.fit(
@@ -215,14 +201,8 @@ ranker = ranker.fit(
     group=session_lengths_train,
 )
 
-test = pl.read_parquet('./data/test/test.parquet')
-test = test.with_columns([
-    pl.col('session').cast(pl.datatypes.Int32),
-    pl.col('type').cast(pl.datatypes.UInt8),
-    pl.col('aid').cast(pl.datatypes.Int32),
-    pl.col('ts').cast(pl.datatypes.Int64)
-])
 test = apply(test, pipeline)
+print(f'{test.columns=}')
 
 scores = ranker.predict(test[feature_cols].to_pandas())
 
@@ -241,5 +221,5 @@ for session, preds in zip(test_predictions['session'].to_numpy(), test_predictio
         session_types.append(f'{session}_{session_type}')
 
 submission = pl.DataFrame({'session_type': session_types, 'labels': labels})
-# submission.write_csv('submission.csv')
+submission.write_csv('submission.csv')
 
