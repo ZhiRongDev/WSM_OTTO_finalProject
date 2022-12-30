35b909a544146330efd9ec0dc7d7e1fc442b9d2d
zhi-rong <jordan990301@gmail.com>
2022-12-30 23:47:00 +0800

    main.py is cudf + w2vec + lgbm with pipline

--- main.py
+++ main.py
@@ -1,142 +1,225 @@
+import time
+import pandas as pd
 import polars as pl
 import numpy as np
+from collections import defaultdict
+import cudf
+cudf.set_option("default_integer_bitwidth", 32)
+cudf.set_option("default_float_bitwidth", 32)
+
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
 
-from sklearn.cluster import MiniBatchKMeans, kmeans_plusplus
-# from sklearn.decomposition import PCA
-
-
-
+# train = pl.read_parquet('./data/local_validation/test.parquet')
 train = pl.read_parquet('./data/test/train.parquet')
-test = pl.read_parquet('./data/test/test.parquet')
+test = pl.read_parquet('./data/local_validation/test.parquet')
 
+train = train.with_columns([
+    pl.col('session').cast(pl.datatypes.Int32),
+    pl.col('type').cast(pl.datatypes.UInt8),
+    pl.col('aid').cast(pl.datatypes.Int32),
+    pl.col('ts').cast(pl.datatypes.Int64)
+])
+
+test = test.with_columns([
+    pl.col('session').cast(pl.datatypes.Int32),
+    pl.col('type').cast(pl.datatypes.UInt8),
+    pl.col('aid').cast(pl.datatypes.Int32),
+    pl.col('ts').cast(pl.datatypes.Int64)
+])
+
+##### for Word2Vec pretrain
 sentences_df = pl.concat([train, test]).groupby('session').agg(
     pl.col('aid').alias('sentence')
 )
 
 sentences = sentences_df['sentence'].to_list()
 
-w2vec = Word2Vec(sentences=sentences, vector_size=100, min_count=1, workers=4)
+print("w2vec start")
+start = time.time()
 
-def vectorize_list(list_of_session):
-    features = []     
+w2vec = Word2Vec(sentences=sentences, vector_size=32, min_count=1, workers=4)
 
-    for session in list_of_session['session']:
-        vectors = []
-        tokens = list(list_of_session.filter(pl.col("session") == session)["sentence"][0])
-        
-        for token in tokens:
-            vectors.append(w2vec.wv[token])
+print("w2vec end")
+end = time.time()
+print(f'執行時間: {end - start} 秒\n')
+
+from annoy import AnnoyIndex
+
+aid2idx = {aid: i for i, aid in enumerate(w2vec.wv.index_to_key)}
+index = AnnoyIndex(32, 'euclidean')
+
+for aid, idx in aid2idx.items():
+    index.add_item(idx, w2vec.wv.vectors[idx])
     
+index.build(10)
+#####
 
-        vectors = np.asarray(vectors)
-        avg_vec = vectors.mean(axis=0)
-        features.append(avg_vec)
+###
+train_labels = pl.read_parquet('./data/local_validation/test_labels.parquet')
 
-    return features
-
-def vectorize_dict(list_of_session):
-    features = {}
-
-    for session in list_of_session['session']:
-
-        vectors = []
-        tokens = list(list_of_session.filter(pl.col("session") == session)["sentence"][0])
-        
-        for token in tokens:
-            vectors.append(w2vec.wv[token])
+def word2vec_candidate(df): 
+    global index
+    session_types = ['clicks', 'carts', 'orders']
+    df_session_AIDs = df.to_pandas().reset_index(drop=True).groupby('session')['aid'].apply(list)
+    df_session_types = df.to_pandas().reset_index(drop=True).groupby('session')['type'].apply(list)
     
+    candidates = cudf.DataFrame({"session": [], "aid":[]})
 
-        vectors = np.asarray(vectors)
-        avg_vec = vectors.mean(axis=0)
-        features[session] = avg_vec
+    print("candidate calc start")
+    start = time.time()
 
-    return features
+    type_weight_multipliers = {0: 1, 1: 6, 2: 3}
+    for idx, (AIDs, types) in enumerate(zip(df_session_AIDs, df_session_types)):
+        session_num = df_session_AIDs.index[idx]    
+        # print(session_num)
+        if len(AIDs) >= 20:
+            # if we have enough aids (over equals 20) we don't need to look for candidates! we just use the old logic
+            weights=np.logspace(0.1,1,len(AIDs),base=2, endpoint=True)-1
+            aids_temp=defaultdict(lambda: 0)
+            for aid,w,t in zip(AIDs,weights,types): 
+                aids_temp[aid]+= w * type_weight_multipliers[t]
+                
+            sorted_aids=[k for k, v in sorted(aids_temp.items(), key=lambda item: -item[1])]
 
-# sentences_vec = vectorize_list(sentences_df)
-sentences_vec_dict = vectorize_dict(sentences_df)
+            if len(sorted_aids) < 20:
+                AIDs = list(dict.fromkeys(AIDs[::-1]))
+                # let's grab the most recent aid
+                most_recent_aid = AIDs[0]
+                # and look for some neighbors!
+                nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
+                sorted_aids = (sorted_aids + nns)[:20]
 
-# kmeans = MiniBatchKMeans().fit(sentences_vec)
+            session_arr = np.full((20), session_num)        
 
-# label_df = pl.DataFrame({
-#     "label": kmeans.labels_
-# })
+            session_df = cudf.DataFrame({
+                "session": session_arr,
+                "aid": sorted_aids[:20] 
+            })
+            candidates = cudf.concat([candidates, session_df])
+        else:
+            # here we don't have 20 aids to output -- we will use word2vec embeddings to generate candidates!
+            AIDs = list(dict.fromkeys(AIDs[::-1]))
+            # let's grab the most recent aid
+            most_recent_aid = AIDs[0]
+            # and look for some neighbors!
+            nns = [w2vec.wv.index_to_key[i] for i in index.get_nns_by_item(aid2idx[most_recent_aid], 21)[1:]]
+            session_arr = np.full((20), session_num)
+            session_df = cudf.DataFrame({
+                "session": session_arr,
+                "aid": (AIDs+nns)[:20] 
+            })
+            
+            candidates = cudf.concat([candidates, session_df])
 
-# sentences_df = pl.concat([sentences_df, label_df], how="horizontal")
+    print("candidate calc end")
+    end = time.time()
+    print(f'執行時間: {end - start} 秒\n')
 
-########
-from gensim.similarities.annoy import AnnoyIndexer
+    candidates = candidates.to_pandas()
+    candidates = pl.DataFrame(candidates)
+    candidates = candidates.with_column(pl.col('aid').cumcount().over('session').alias('word2vec_rank') + 1)
+    candidates = candidates.with_columns([
+        pl.col('session').cast(pl.datatypes.Int32),
+    ])
+     
+    df = df.join(candidates, on=['session', 'aid'], how='outer').sort("session")
+    return df
 
-# 50 trees are being used in this example
-annoy_index = AnnoyIndexer(w2vec, 50)
+def add_action_num_reverse_chrono(df):
+    return df.select([
+        pl.col('*'),
+        pl.col('session').cumcount().reverse().over('session').alias('action_num_reverse_chrono')
+    ])
 
-########
+def add_session_length(df):
+    return df.select([
+        pl.col('*'),
+        pl.col('session').count().over('session').alias('session_length')
+    ])
 
-import pandas as pd
-import numpy as np
+def add_log_recency_score(df):
+    linear_interpolation = 0.1 + ((1-0.1) / (df['session_length']-1)) * (df['session_length']-df['action_num_reverse_chrono']-1)
+    return df.with_columns(pl.Series(2**linear_interpolation - 1).alias('log_recency_score')).fill_nan(1)
 
-from collections import defaultdict
+def add_type_weighted_log_recency_score(df):
+    type_weights = {0:1, 1:6, 2:3}
+    type_weighted_log_recency_score = pl.Series(df['log_recency_score'] / df['type'].apply(lambda x: type_weights[x]))
+    return df.with_column(type_weighted_log_recency_score.alias('type_weighted_log_recency_score'))
 
-sample_sub = pd.read_csv('./data/sample_submission.csv')
+def apply(df, pipeline):
+    for f in pipeline:
+        df = f(df)
+    return df
 
-session_types = ['clicks', 'carts', 'orders']
-test_session_AIDs = test.to_pandas().reset_index(drop=True).groupby('session')['aid'].apply(list)
-test_session_types = test.to_pandas().reset_index(drop=True).groupby('session')['type'].apply(list)
+pipeline = [add_action_num_reverse_chrono, add_session_length, add_log_recency_score, add_type_weighted_log_recency_score, word2vec_candidate]
 
+train = apply(train, pipeline)
+
+# print(train.columns)
+
+type2id = {"clicks": 0, "carts": 1, "orders": 2}
+
+train_labels = train_labels.explode('ground_truth').with_columns([
+    pl.col('ground_truth').alias('aid'),
+    pl.col('type').apply(lambda x: type2id[x])
+])[['session', 'type', 'aid']]
+
+train_labels = train_labels.with_columns([
+    pl.col('session').cast(pl.datatypes.Int32),
+    pl.col('type').cast(pl.datatypes.UInt8),
+    pl.col('aid').cast(pl.datatypes.Int32)
+])
+
+train_labels = train_labels.with_column(pl.lit(1).alias('gt'))
+
+train = train.join(train_labels, how='left', on=['session', 'type', 'aid']).with_column(pl.col('gt').fill_null(0))
+
+def get_session_lenghts(df):
+    return df.groupby('session').agg([
+        pl.col('session').count().alias('session_length')
+    ])['session_length'].to_numpy()
+
+session_lengths_train = get_session_lenghts(train)
+
+from lightgbm.sklearn import LGBMRanker
+
+ranker = LGBMRanker(
+    objective="lambdarank",
+    metric="ndcg",
+    boosting_type="dart",
+    n_estimators=20,
+    importance_type='gain',
+)
+
+feature_cols = ['aid', 'type', 'action_num_reverse_chrono', 'session_length', 'log_recency_score', 'type_weighted_log_recency_score', 'word2vec_rank']
+target = 'gt'
+
+ranker = ranker.fit(
+    train[feature_cols].to_pandas(),
+    train[target].to_pandas(),
+    group=session_lengths_train,
+)
+
+### create submission
+test = apply(test, pipeline)
+print(f'{test.columns=}')
+
+scores = ranker.predict(test[feature_cols].to_pandas())
+
+test = test.with_columns(pl.Series(name='score', values=scores))
+test_predictions = test.sort(['session', 'score'], reverse=True).groupby('session').agg([
+    pl.col('aid').limit(20).list()
+])
+
+session_types = []
 labels = []
 
-type_weight_multipliers = {0: 1, 1: 6, 2: 3}
-for idx, (AIDs, types) in enumerate(zip(test_session_AIDs, test_session_types)):
-    
-    session_num = test_session_AIDs.index[idx]
+for session, preds in zip(test_predictions['session'].to_numpy(), test_predictions['aid'].to_numpy()):
+    l = ' '.join(str(p) for p in preds)
+    for session_type in ['clicks', 'carts', 'orders']:
+        labels.append(l)
+        session_types.append(f'{session}_{session_type}')
 
-    # print(idx, AIDs, types)
-    # print(test_session_AIDs.index[idx])
-   
-    # cluster_num = sentences_df.filter(pl.col("session") == session_num).select("label")
-    # cluster_num =list(cluster_num)[0][0]
-    
-    if len(AIDs) >= 20:
-        # if we have enough aids (over equals 20) we don't need to look for candidates! we just use the old logic
-        weights=np.logspace(0.1,1,len(AIDs),base=2, endpoint=True)-1
-        aids_temp=defaultdict(lambda: 0)
-        for aid,w,t in zip(AIDs,weights,types): 
-            aids_temp[aid]+= w * type_weight_multipliers[t]
-            
-        sorted_aids=[k for k, v in sorted(aids_temp.items(), key=lambda item: -item[1])]
-        labels.append(sorted_aids[:20])
-    else:
-        # here we don't have 20 aids to output -- we will use word2vec embeddings to generate candidates!
-        AIDs = list(dict.fromkeys(AIDs[::-1]))
-        
-        # let's grab the most recent aid
-        most_recent_aid = AIDs[0]
-             
-        # and look for some neighbors!
-        # vector = w2vec.wv[most_recent_aid]
-        
-        ## use the centeri in cluster
-        # vector = kmeans.cluster_centers_[cluster_num]
-        
-        ## use the represent item in session
-        vector = sentences_vec_dict[session_num]
-
-        nns = [i for i, j in w2vec.wv.most_similar([vector], topn=21, indexer=annoy_index)]
-        nns.pop(0)
-
-        labels.append((AIDs+nns)[:20])
-
-labels_as_strings = [' '.join([str(l) for l in lls]) for lls in labels]
-
-predictions = pd.DataFrame(data={'session_type': test_session_AIDs.index, 'labels': labels_as_strings})
-
-prediction_dfs = []
-
-for st in session_types:
-    modified_predictions = predictions.copy()
-    modified_predictions.session_type = modified_predictions.session_type.astype('str') + f'_{st}'
-    prediction_dfs.append(modified_predictions)
-
-submission = pd.concat(prediction_dfs).reset_index(drop=True)
-submission.to_csv('submission.csv', index=False)
+submission = pl.DataFrame({'session_type': session_types, 'labels': labels})
+submission.write_csv('submission.csv')
